4d97a1de857bc5362f789946f4b67749cd863ccc
Andrew Dibb <andrewdibb@gmail.com>
2016-10-07 17:41:10 -0500

    inital commit and finished tutorial

---
+++ config/auth.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '1589726971330178', // your App ID
+        'clientSecret'  : 'a072a64483d032d9c8c824eb2a0410d1', // your App Secret
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+    },
+
+    'twitterAuth' : {
+        'consumerKey'       : 'wCrmZZx2XRC9wv2P7NYJT7onu',
+        'consumerSecret'    : 'aReyxpYY5mffjPMm6QR908KnHZrgKzF1yNwUedSSnbkZaPSbzV',
+        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'      : '953152630132-kqb5q0t47kkbnbjgr66sutb0kgskn3kd.apps.googleusercontent.com',
+        'clientSecret'  : '30cGRsj7TD-2jzBVDeYhsOzz',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    }
+
+};
